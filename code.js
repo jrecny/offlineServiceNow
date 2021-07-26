8fe4e71e4134f62095d6323db3a6ddb76ff8f9d0
John Recny <jrecny@gmail.com>
2021-07-26 13:31:54 -0400

    gather user data

---
+++ code.js
@@ -0,0 +1,35 @@
+const btn = document.getElementById('btn');
+btn.addEventListener('click', getFormData);
+//GATHER FORM DATA FOR USER
+
+function getFormData () {
+	// User Info Obj
+	const user = {
+		netID       : document.getElementById('netID').value,
+		contact     : document.getElementById('contact').value,
+		location    : document.getElementById('location').value,
+		description : document.getElementById('issue').value,
+		team        : document.getElementById('team').value
+	};
+
+	// Get priority level
+	const rbs = document.querySelectorAll('input[name=radioBtn]');
+	let selectedValue;
+	for (const rb of rbs) {
+		if (rb.checked) {
+			selectedValue = rb.value;
+			break;
+		}
+	}
+	// alert(selectedValue); /*test priority*/
+
+	/*Get category*/
+	const category = {
+		cat : document.getElementById('cat').value,
+		sub : document.getElementById('sub').value
+	};
+	// alert(category.sub);
+
+	var blob = new Blob([ 'Hello, world!' ], { type: 'text/plain;charset=utf-8' });
+	FileSaver.saveAs(blob, 'hello world.txt');
+}
