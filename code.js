6c372c004fe41cdbef157b5505d664c18aa5bd02
John Recny <jrecny@gmail.com>
2021-07-26 21:36:19 -0400

    format save function

--- code.js
+++ code.js
@@ -1,35 +1,36 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', getFormData);
+
+let today = new Date();
+let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
 //GATHER FORM DATA FOR USER
+function getFormData () {
+	// User Info Obj
+	const user = {
+		netID       : document.getElementById('netID').value,
+		contact     : document.getElementById('contact').value,
+		location    : document.getElementById('location').value,
+		description : document.getElementById('issue').value,
+		team        : document.getElementById('team').value
+	};
 
-function getFormData() {
-    // User Info Obj
-    const user = {
-        netID: document.getElementById('netID').value,
-        contact: document.getElementById('contact').value,
-        location: document.getElementById('location').value,
-        description: document.getElementById('issue').value,
-        team: document.getElementById('team').value
-    };
+	// Get priority level
+	const rbs = document.querySelectorAll('input[name=radioBtn]');
+	let selectedValue;
+	for (const rb of rbs) {
+		if (rb.checked) {
+			selectedValue = rb.value;
+			break;
+		}
+	}
 
-    // Get priority level
-    const rbs = document.querySelectorAll('input[name=radioBtn]');
-    let selectedValue;
-    for (const rb of rbs) {
-        if (rb.checked) {
-            selectedValue = rb.value;
-            break;
-        }
-    }
-    // alert(selectedValue); /*test priority*/
+	/*Get category*/
+	const category = {
+		cat : document.getElementById('cat').value,
+		sub : document.getElementById('sub').value
+	};
 
-    /*Get category*/
-    const category = {
-        cat: document.getElementById('cat').value,
-        sub: document.getElementById('sub').value
-    };
-    // alert(category.sub);
-
-    var blob = new Blob([user.netID], { type: 'text/plain;charset=utf-8' });
-    saveAs(blob, 'hello world.txt');
+	var blob = new Blob([ user ], { type: 'text/plain;charset=utf-8' });
+	saveAs(blob, 'ServiceNow Ticket Time' + ' ' + time);
 }
